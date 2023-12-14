528c9507211e6bdb4b064fde32161540bc337ad8
Willian Pagane <willianmazini@gmail.com>
2023-12-13 21:21:12 -0300

    initial release

---
+++ script.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const opcoesDeEstudoBase = [
+    "Programação Rocketseat",
+    "Programação Low Code",
+    "Finclass"
+  ];
+
+  const coresDoBotao = [
+    "#50C878", // Verde Esmeralda
+    "#9966CC", // Roxo Ametista
+    "#FF7F50", // Laranja Coral
+    "#FF69B4"  // Rosa Choque
+  ];
+
+  const marioKartSwitch = document.getElementById('marioKartSwitch');
+  const botao = document.querySelector('.button a');
+  let foiClicado = false;
+  let ultimaOpcao = "";
+  let ultimaCor = "";
+
+  botao.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (!foiClicado) {
+      document.documentElement.style.setProperty('--text-color', '#050f1a');
+      botao.classList.add('button-hover-final');
+      foiClicado = true;
+    }
+
+    // Inclui "Mario Kart" nas opções se o switch estiver ativo
+    let opcoesDeEstudo = marioKartSwitch.checked ? [...opcoesDeEstudoBase, "Mario Kart", "Mario Kart", "Mario Kart"] : [...opcoesDeEstudoBase];
+
+    let novasOpcoes = ultimaOpcao ? opcoesDeEstudo.filter(opcao => opcao !== ultimaOpcao) : opcoesDeEstudo;
+    let novasCores = ultimaCor ? coresDoBotao.filter(cor => cor !== ultimaCor) : coresDoBotao;
+
+    const novoTexto = novasOpcoes[Math.floor(Math.random() * novasOpcoes.length)];
+    const novaCor = novasCores[Math.floor(Math.random() * novasCores.length)];
+
+    ultimaOpcao = novoTexto;
+    ultimaCor = novaCor;
+
+    requestAnimationFrame(() => {
+      setTimeout(() => {
+        botao.textContent = novoTexto;
+        document.documentElement.style.setProperty('--button-color', novaCor);
+      }, 300);
+    });
+  });
+
+  botao.addEventListener('transitionend', () => {
+    if (!foiClicado) {
+      document.documentElement.style.setProperty('--text-color', '#050f1a');
+    }
+  });
+});
